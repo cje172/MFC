1ca354553636da6e1bbde323b9ab6dadf4827d70
Jieun Choi <cje172@naver.com>
2022-05-16 14:48:28 +0900

    FormView 이용 문자 출력 / 파일 입출력

---
+++ Practice10_1/Practice10_1/Practice10_1View.cpp
@@ -0,0 +1,315 @@
+﻿
+// Practice10_1View.cpp: CPractice101View 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "Practice10_1.h"
+#endif
+
+#include "Practice10_1Doc.h"
+#include "Practice10_1View.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CPractice101View
+
+IMPLEMENT_DYNCREATE(CPractice101View, CFormView)
+
+BEGIN_MESSAGE_MAP(CPractice101View, CFormView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CFormView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CFormView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CPractice101View::OnFilePrintPreview)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+	ON_BN_CLICKED(IDC_BUTTON_OUTPUT, &CPractice101View::OnClickedButtonOutput)
+	ON_BN_CLICKED(IDC_BUTTON_COLOR, &CPractice101View::OnClickedButtonColor)
+	ON_BN_CLICKED(IDC_BUTTON_RESET, &CPractice101View::OnClickedButtonReset)
+	ON_WM_HSCROLL()
+	ON_EN_CHANGE(IDC_EDIT_RED, &CPractice101View::OnChangeEditRed)
+	ON_EN_CHANGE(IDC_EDIT_GREEN, &CPractice101View::OnChangeEditGreen)
+	ON_EN_CHANGE(IDC_EDIT_BLUE, &CPractice101View::OnChangeEditBlue)
+	ON_EN_CHANGE(IDC_EDIT_TEXT, &CPractice101View::OnChangeEditText)
+	
+END_MESSAGE_MAP()
+
+// CPractice101View 생성/소멸
+
+CPractice101View::CPractice101View() noexcept
+	: CFormView(IDD_PRACTICE10_1_FORM)
+	, m_strText(_T(""))
+	, m_nRed(0)
+	, m_nGreen(0)
+	, m_nBlue(0)
+	, m_nX(0)
+	, m_nY(0)
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+	m_colorText = RGB(0, 0, 0);
+	m_nX = 30;
+	m_nY = 270;  // 대화상자의 크기와 배치된 컨트롤에 따라 변경될 수 있다.
+}
+
+CPractice101View::~CPractice101View()
+{
+}
+
+void CPractice101View::DoDataExchange(CDataExchange* pDX)
+{
+	CFormView::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT_TEXT, m_strText);
+	DDX_Text(pDX, IDC_EDIT_RED, m_nRed);
+	DDV_MinMaxInt(pDX, m_nRed, 0, 255);
+	DDX_Text(pDX, IDC_EDIT_GREEN, m_nGreen);
+	DDV_MinMaxInt(pDX, m_nGreen, 0, 255);
+	DDX_Text(pDX, IDC_EDIT_BLUE, m_nBlue);
+	DDV_MinMaxInt(pDX, m_nBlue, 0, 255);
+	DDX_Text(pDX, IDC_EDIT_X, m_nX);
+	DDX_Text(pDX, IDC_EDIT_Y, m_nY);
+
+	DDX_Control(pDX, IDC_SLIDER_X, m_sliderX);
+	DDX_Control(pDX, IDC_SLIDER_Y, m_sliderY);
+	DDX_Control(pDX, IDC_SLIDER_X, m_sliderX);
+	DDX_Control(pDX, IDC_SLIDER_Y, m_sliderY);
+}
+
+BOOL CPractice101View::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CFormView::PreCreateWindow(cs);
+}
+
+void CPractice101View::OnInitialUpdate()
+{
+	CFormView::OnInitialUpdate();
+	GetParentFrame()->RecalcLayout();
+	ResizeParentToFit();
+
+	m_sliderX.SetRange(20, 300);
+	m_sliderY.SetRange(260, 500);  // 범위는 대화상자의 크기와 컨트롤의 배치에 따라
+	m_sliderX.SetPos(30);
+	m_sliderY.SetPos(270);
+}
+
+
+// CPractice101View 인쇄
+
+
+void CPractice101View::OnFilePrintPreview()
+{
+#ifndef SHARED_HANDLERS
+	AFXPrintPreview(this);
+#endif
+}
+
+BOOL CPractice101View::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CPractice101View::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CPractice101View::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+void CPractice101View::OnPrint(CDC* pDC, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 여기에 사용자 지정 인쇄 코드를 추가합니다.
+}
+
+void CPractice101View::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CPractice101View::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CPractice101View 진단
+
+#ifdef _DEBUG
+void CPractice101View::AssertValid() const
+{
+	CFormView::AssertValid();
+}
+
+void CPractice101View::Dump(CDumpContext& dc) const
+{
+	CFormView::Dump(dc);
+}
+
+CPractice101Doc* CPractice101View::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CPractice101Doc)));
+	return (CPractice101Doc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CPractice101View 메시지 처리기
+
+
+
+
+
+void CPractice101View::OnClickedButtonOutput()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData(TRUE);
+	m_colorText = RGB(m_nRed, m_nGreen, m_nBlue);  // 입력된 색상을 문자 색상으로 저장
+	// 잘못된 색상일 경우 에러메시지를 보낸다.
+	if (m_nRed > 255 || m_nRed < 0 || m_nGreen > 255 || m_nGreen < 0 ||
+		m_nBlue > 255 || m_nBlue < 0)
+	{
+		AfxMessageBox(_T("잘못된 색상입니다."));
+	}
+	else
+	{
+		Invalidate();
+	}
+}
+
+
+void CPractice101View::OnClickedButtonColor()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	// 색상 다이얼로그를 띄운다.
+	CColorDialog colorDlg;
+	if (colorDlg.DoModal() == IDOK)
+	{
+		m_colorText = colorDlg.GetColor();
+	}
+	// 색상을 나타내는 RGB EditBox에 각 색의 RGB 값을 넣어준다.
+	m_nRed = GetRValue(m_colorText);
+	m_nGreen = GetGValue(m_colorText);
+	m_nBlue = GetBValue(m_colorText);
+
+	UpdateData(FALSE);
+	Invalidate();
+	GetDocument()->SetModifiedFlag();
+}
+
+
+void CPractice101View::OnClickedButtonReset()
+{
+	// TODO: 여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	m_strText.Empty();
+	m_nRed = 0;
+	m_nGreen = 0;
+	m_nBlue = 0;
+	m_nX = 30;
+	m_nY = 270;
+	m_sliderX.SetPos(30);
+	m_sliderY.SetPos(270);
+	UpdateData(FALSE);
+	Invalidate();
+	GetDocument()->OnNewDocument();
+}
+
+
+void CPractice101View::OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar)
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
+	int nX = m_sliderX.GetPos();
+	int nY = m_sliderY.GetPos();
+
+	if (pScrollBar->GetSafeHwnd() == m_sliderX.m_hWnd)
+		m_nX = nX;
+	else if (pScrollBar->GetSafeHwnd() == m_sliderY.m_hWnd)
+		m_nY = nY;
+	else
+		return;
+
+	UpdateData(FALSE);
+	Invalidate();
+	CFormView::OnHScroll(nSBCode, nPos, pScrollBar);
+}
+
+
+void CPractice101View::OnChangeEditRed()
+{
+	// TODO:  RICHEDIT 컨트롤인 경우, 이 컨트롤은
+	// CFormView::OnInitDialog() 함수를 재지정 
+	//하고 마스크에 OR 연산하여 설정된 ENM_CHANGE 플래그를 지정하여 CRichEditCtrl().SetEventMask()를 호출하지 않으면
+	// 이 알림 메시지를 보내지 않습니다.
+
+	// TODO:  여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData(TRUE);
+	m_colorText = RGB(m_nRed, m_nGreen, m_nBlue);
+	Invalidate();
+	GetDocument()->SetModifiedFlag();
+}
+
+
+void CPractice101View::OnChangeEditGreen()
+{
+	// TODO:  RICHEDIT 컨트롤인 경우, 이 컨트롤은
+	// CFormView::OnInitDialog() 함수를 재지정 
+	//하고 마스크에 OR 연산하여 설정된 ENM_CHANGE 플래그를 지정하여 CRichEditCtrl().SetEventMask()를 호출하지 않으면
+	// 이 알림 메시지를 보내지 않습니다.
+
+	// TODO:  여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData(TRUE);
+	m_colorText = RGB(m_nRed, m_nGreen, m_nBlue);
+	Invalidate();
+	GetDocument()->SetModifiedFlag();
+}
+
+
+void CPractice101View::OnChangeEditBlue()
+{
+	// TODO:  RICHEDIT 컨트롤인 경우, 이 컨트롤은
+	// CFormView::OnInitDialog() 함수를 재지정 
+	//하고 마스크에 OR 연산하여 설정된 ENM_CHANGE 플래그를 지정하여 CRichEditCtrl().SetEventMask()를 호출하지 않으면
+	// 이 알림 메시지를 보내지 않습니다.
+
+	// TODO:  여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData(TRUE);
+	m_colorText = RGB(m_nRed, m_nGreen, m_nBlue);
+	Invalidate();
+	GetDocument()->SetModifiedFlag();
+}
+
+
+void CPractice101View::OnChangeEditText()
+{
+	// TODO:  RICHEDIT 컨트롤인 경우, 이 컨트롤은
+	// CFormView::OnInitDialog() 함수를 재지정 
+	//하고 마스크에 OR 연산하여 설정된 ENM_CHANGE 플래그를 지정하여 CRichEditCtrl().SetEventMask()를 호출하지 않으면
+	// 이 알림 메시지를 보내지 않습니다.
+
+	// TODO:  여기에 컨트롤 알림 처리기 코드를 추가합니다.
+	UpdateData(TRUE);
+	GetDocument()->SetModifiedFlag();
+}
+
+
+void CPractice101View::OnDraw(CDC* pDC)
+{
+	// TODO: 여기에 특수화된 코드를 추가 및/또는 기본 클래스를 호출합니다.
+	pDC->SetTextColor(m_colorText);
+	pDC->TextOut(m_nX, m_nY, m_strText);
+	m_sliderX.SetPos(m_nX);
+	m_sliderY.SetPos(m_nY);
+}
---
+++ Practice10_1/Practice10_1/Practice10_1View.h
@@ -0,0 +1,78 @@
+﻿
+// Practice10_1View.h: CPractice101View 클래스의 인터페이스
+//
+
+#pragma once
+
+
+class CPractice101View : public CFormView
+{
+protected: // serialization에서만 만들어집니다.
+	CPractice101View() noexcept;
+	DECLARE_DYNCREATE(CPractice101View)
+
+public:
+#ifdef AFX_DESIGN_TIME
+	enum{ IDD = IDD_PRACTICE10_1_FORM };
+#endif
+
+// 특성입니다.
+public:
+	CPractice101Doc* GetDocument() const;
+
+// 작업입니다.
+public:
+
+// 재정의입니다.
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void DoDataExchange(CDataExchange* pDX);    // DDX/DDV 지원입니다.
+	virtual void OnInitialUpdate(); // 생성 후 처음 호출되었습니다.
+	virtual BOOL OnPreparePrinting(CPrintInfo* pInfo);
+	virtual void OnBeginPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnEndPrinting(CDC* pDC, CPrintInfo* pInfo);
+	virtual void OnPrint(CDC* pDC, CPrintInfo* pInfo);
+
+// 구현입니다.
+public:
+	virtual ~CPractice101View();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 생성된 메시지 맵 함수
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+public:
+	CString m_strText;
+	int m_nRed;
+	int m_nGreen;
+	int m_nBlue;
+	int m_nX;
+	int m_nY;
+	CSliderCtrl m_sliderX;
+	CSliderCtrl m_sliderY;
+	COLORREF m_colorText;
+	afx_msg void OnClickedButtonOutput();
+	afx_msg void OnClickedButtonColor();
+	afx_msg void OnClickedButtonReset();
+	afx_msg void OnHScroll(UINT nSBCode, UINT nPos, CScrollBar* pScrollBar);
+	afx_msg void OnChangeEditRed();
+	afx_msg void OnChangeEditGreen();
+	afx_msg void OnChangeEditBlue();
+	afx_msg void OnChangeEditText();
+	virtual void OnDraw(CDC* pDC);
+};
+
+#ifndef _DEBUG  // Practice10_1View.cpp의 디버그 버전
+inline CPractice101Doc* CPractice101View::GetDocument() const
+   { return reinterpret_cast<CPractice101Doc*>(m_pDocument); }
+#endif
+
